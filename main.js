497333caaa85787738909fc2d9ff867fae715304
lonqdh <109335467+lonqdh@users.noreply.github.com>
2022-11-30 09:21:01 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -8,6 +8,6 @@ app.get('/',(req,res)=>{
     res.render('home')
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 app.listen(PORT)
 console.log("Server is running...")
